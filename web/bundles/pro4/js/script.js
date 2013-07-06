49b714d9f7c0d16ed8d7cf5a815033b56a012d1a
Iris Schaffer <irisSchaffer@gmx.net>
2013-07-06 18:54:11 +0200

    pushing web/bundles data

---
+++ web/bundles/pro4/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.colorSelector').ColorPicker({
+		color: '#0000ff',
+		onShow: function (colpkr) {
+			$(colpkr).fadeIn(500);
+			return false;
+		},
+		onHide: function (colpkr) {
+			$(colpkr).fadeOut(500);
+			return false;
+		},
+		onChange: function (hsb, hex, rgb) {
+			$('.colorSelector div').css('backgroundColor', '#' + hex);
+			$('.colorSelector').val(hex);
+		}
+	});
+});
