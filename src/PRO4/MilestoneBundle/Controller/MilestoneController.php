5ffbece485041a092403c1122cf4a4b707b9254e
Iris Schaffer <irisSchaffer@gmx.net>
2013-07-11 10:06:40 +0200

    Fixed Milestone-Edit

--- src/PRO4/MilestoneBundle/Controller/MilestoneController.php
+++ src/PRO4/MilestoneBundle/Controller/MilestoneController.php
@@ -15,17 +15,29 @@ use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
 use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 
+use IllegalArgumentException;
+
 class MilestoneController extends MyController {
    	
    	public function addAction(Request $request, $projectId, $milestonePlanId) {
-   		$project = $this->find("PRO4\ProjectBundle\Entity\Project", $projectId);
    		$milestonePlan = $this->find("PRO4\MilestoneBundle\Entity\MilestonePlan", $milestonePlanId);
-   		$this->checkPermission("EDIT", $project);
    		
    		$milestone = new Milestone();
    		$milestone->setMilestonePlan($milestonePlan);
    		$milestone->setStartDate($milestonePlan->getStartDate());
    		$milestone->setEndDate($milestonePlan->getEndDate());
+
+	    return $this->milestoneAction($projectId, $milestonePlan, $milestone, $request);
+   	}
+   	
+   	private function milestoneAction($projectId, MilestonePlan $milestonePlan, Milestone $milestone, Request $request) {
+   		$project = $this->find("PRO4\ProjectBundle\Entity\Project", $projectId);
+   		
+   		$this->checkPermission("EDIT", $project);
+   		
+   		if($milestonePlan->getProject() !== $project) {
+   			throw new IllegalArgumentException();
+   		}
    		
    		$form = $this->createForm(new MilestoneType(), $milestone);
    		
@@ -44,33 +56,24 @@ class MilestoneController extends MyController {
 	        }
 	    }
 
-	    return $this->render("PRO4MilestoneBundle:Milestone:milestoneForm.html.twig", array("project" => $project, "action" => "Add", "form" => $form->createView()));	
+	    return $this->render(
+			"PRO4MilestoneBundle:Milestone:milestoneForm.html.twig",
+	    	array(
+				"project" => $project,
+				"action" => "Add",
+				"form" => $form->createView()
+			)
+		);	
    	}
    	
    	public function editAction(Request $request, $projectId, $milestonePlanId, $milestoneId) {
-		$project = $this->find("PRO4\ProjectBundle\Entity\Project", $projectId);
    		$milestonePlan = $this->find("PRO4\MilestoneBundle\Entity\MilestonePlan", $milestonePlanId);
    		$milestone = $this->find("PRO4\MilestoneBundle\Entity\Milestone", $milestoneId);
    		
-   		$this->checkPermission("EDIT", $project);
+   		if($milestone->getMilestonePlan() !== $milestonePlan) {
+   			throw new InvalidArgumentException();
+   		}
    		
-   		$form = $this->createForm(new MilestoneType(), $milestone, array("disabled" => true));
-   		
-   		if ($request->isMethod("POST")) {   			
-	        $form->bind($request);
-
-	        if ($form->isValid()) {
-   				$this->persist($milestone);
-			
-    			$this->get('session')->getFlashBag()->add(
-				    'success',
-				    'You successfully added a milestone!'
-				);
-
-				return $this->redirect($this->generateUrl("milestone_plan", array("id" => $projectId)));
-	        }
-	    }
-
-	    return $this->render("PRO4MilestoneBundle:Milestone:milestoneForm.html.twig", array("project" => $project, "action" => "Add", "form" => $form->createView()));	
+	    return $this->milestoneAction($projectId, $milestonePlan, $milestone, $request);
 	}
 }
